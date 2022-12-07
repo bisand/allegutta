9e4a479eae7bf2ed6b43079129c59a27e2999ba5
André Biseth <andre@biseth.net>
2022-12-07 07:07:36 +0000

    Added Yahoo component for market data

--- AlleGutta.Console/Program.cs
+++ AlleGutta.Console/Program.cs
@@ -3,7 +3,9 @@ using AlleGutta.Models;
 using AlleGutta.Nordnet;
 using AlleGutta.Repository;
 using AlleGutta.Repository.Database.Configuration;
+using AlleGutta.Yahoo;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 internal static class Program
 {
@@ -24,34 +26,37 @@ internal static class Program
             DatabaseConfiguration.UpdateDatabase(scope.ServiceProvider);
         }
 
-        var username = Environment.GetEnvironmentVariable("NORDNET_USERNAME") ?? string.Empty;
-        var password = Environment.GetEnvironmentVariable("NORDNET_PASSWORD") ?? string.Empty;
+        // var username = Environment.GetEnvironmentVariable("NORDNET_USERNAME") ?? string.Empty;
+        // var password = Environment.GetEnvironmentVariable("NORDNET_PASSWORD") ?? string.Empty;
 
-        var nordnetProcessor = new NordnetWebScraper(new("https://www.nordnet.no/login-next", username, password));
-        var data = await nordnetProcessor.GetBatchData();
+        // var nordnetProcessor = new NordnetWebScraper(new("https://www.nordnet.no/login-next", username, password));
+        // var data = await nordnetProcessor.GetBatchData();
 
-        var portfolio = new Portfolio()
-        {
-            Name = "AlleGutta",
-            Ath = 0,
-            Cash = data.AccountInfo?.account_sum?.value ?? 0,
-            MarketValue = data.AccountInfo?.full_marketvalue?.value ?? 0,
-            Positions = data.Positions?.Select(pos =>
-            {
-                return new PortfolioPosition()
-                {
-                    Symbol = pos.instrument?.symbol,
-                    Name = pos.instrument?.name,
-                    Shares = (int)pos.qty,
-                    AvgPrice = pos.acq_price?.value ?? 0
-                };
-            }).ToArray()
-        };
+        // var portfolio = new Portfolio()
+        // {
+        //     Name = "AlleGutta",
+        //     Ath = 0,
+        //     Cash = data.AccountInfo?.account_sum?.value ?? 0,
+        //     MarketValue = data.AccountInfo?.full_marketvalue?.value ?? 0,
+        //     Positions = data.Positions?.Select(pos =>
+        //     {
+        //         return new PortfolioPosition()
+        //         {
+        //             Symbol = pos.instrument?.symbol,
+        //             Name = pos.instrument?.name,
+        //             Shares = (int)pos.qty,
+        //             AvgPrice = pos.acq_price?.value ?? 0
+        //         };
+        //     }).ToArray()
+        // };
 
         var portfolioData = new PortfolioRepository(ConnectionString);
-        await portfolioData.SavePortfolioAsync(portfolio);
+        var yahoo = new Yahoo();
+        // await portfolioData.SavePortfolioAsync(portfolio);
+
         var value = portfolioData.GetPortfolioPositionsAsync("AlleGutta");
-        var valueTask = await value.ToListAsync();
-        Console.WriteLine(valueTask);
+        var positions = await value.ToListAsync();
+        Portfolio portfolio = await yahoo.GetPortfolio(positions);
+        Console.WriteLine(JsonConvert.SerializeObject(portfolio));
     }
 }
---
+++ AlleGutta.Yahoo/Yahoo.cs
@@ -0,0 +1,149 @@
+﻿using System.Web;
+using AlleGutta.Models;
+using Newtonsoft.Json;
+
+namespace AlleGutta.Yahoo;
+public sealed class Yahoo
+{
+    private readonly string quotesUrl;
+    private readonly string chartUrl;
+
+    public Yahoo()
+    {
+        quotesUrl = "https://query2.finance.yahoo.com/v7/finance/quote";
+        chartUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
+    }
+
+    public async Task<Portfolio> GetPortfolio(IEnumerable<PortfolioPosition> positions)
+    {
+        var tickers = "";
+        decimal costValue = 0.0M;
+        if (positions is null)
+        {
+            throw new ArgumentNullException(nameof(positions), "Portfolio positions cannot be null");
+        }
+        else
+        {
+            foreach (var item in positions)
+            {
+                tickers += item.Symbol + ',';
+                costValue += item.Shares * item.AvgPrice;
+            }
+
+            tickers = tickers.TrimEnd(',');
+        }
+
+        var builder = new UriBuilder(quotesUrl)
+        {
+            Port = -1
+        };
+        var query = HttpUtility.ParseQueryString(builder.Query);
+        query["formatted"] = "false";
+        query["lang"] = "nb-NO";
+        query["region"] = "NO";
+        query["symbols"] = tickers;
+        query["fields"] = "shortName,longName,regularMarketChange,regularMarketChangePercent,regularMarketTime,regularMarketPrice,regularMarketDayHigh,regularMarketDayRange,regularMarketDayLow,regularMarketVolume,regularMarketPreviousClose";
+        query["corsDomain"] = "finance.yahoo.com";
+        builder.Query = query.ToString();
+        string url = builder.ToString();
+
+        using var client = new HttpClient();
+        var response = await client.GetStringAsync(url);
+        var quotes = JsonConvert.DeserializeObject<dynamic>(response);
+
+        var portfolio = new Portfolio();
+        // if (quotes && Array.isArray(quotes))
+        // {
+        //     const currentDay = new Date().getDate();
+        //     let newDay: boolean = false;
+        //     quotes.forEach((element: { symbol: any; regularMarketTime: number; longName: string; regularMarketPrice: number; regularMarketChange: number; regularMarketChangePercent: number; regularMarketPreviousClose: number; regularMarketDayHigh: number; regularMarketDayLow: number; }) => {
+        //         const symbol = element.symbol;
+        //         const symbolDate = new Date(element.regularMarketTime * 1000);
+        //         const symbolDay = symbolDate.getDate();
+        //         if (currentDay === symbolDay)
+        //         {
+        //             newDay = true;
+        //         }
+        //         const result = portfolio.positions.find(obj =>
+        //         {
+        //             return obj.symbol === symbol;
+        //         });
+        //         if (result)
+        //         {
+        //             result.name = element.longName;
+        //             result.last_price = element.regularMarketPrice;
+        //             result.change_today = currentDay === symbolDay ? element.regularMarketChange : 0.0;
+        //             result.change_today_percent = currentDay === symbolDay ? element.regularMarketChangePercent : 0.0;
+        //             result.prev_close = element.regularMarketPreviousClose;
+        //             result.cost_value = result.avg_price * result.shares;
+        //             result.current_value = result.last_price * result.shares;
+        //             result.return = result.current_value - result.cost_value;
+        //             if (result.cost_value && result.cost_value !== 0)
+        //             {
+        //                 result.return_percent = (result.return / result.cost_value) *100;
+        //             }
+        //             else
+        //             {
+        //                 result.return_percent = 0;
+        //             }
+
+        //             portfolio.market_value += result.shares * element.regularMarketPrice;
+        //             portfolio.market_value_prev += result.shares * element.regularMarketPreviousClose;
+        //             portfolio.market_value_max += result.shares * element.regularMarketDayHigh;
+        //             portfolio.market_value_min += result.shares * element.regularMarketDayLow;
+        //             portfolio.change_today_total += currentDay === symbolDay ? result.shares * element.regularMarketChange : 0.0;
+        //         }
+        //         portfolio.equity = portfolio.market_value + portfolio.cash;
+        //         portfolio.change_today_percent = (portfolio.change_today_total / portfolio.market_value_prev) * 100;
+        //         portfolio.change_total = portfolio.market_value - portfolio.cost_value;
+        //         portfolio.change_total_percent = (portfolio.change_total / portfolio.cost_value) * 100;
+
+        //         if (!newDay)
+        //         {
+        //             portfolio.change_today_total = 0.0;
+        //             portfolio.change_today_percent = 0.0;
+        //         }
+        //     });
+        // }
+
+        return portfolio;
+    }
+
+    //     async getChartData(symbol: string, range: string, interval: string): Promise<object> {
+    //             const searchParams = { symbol, range, interval, region: 'NO', lang: 'nb-NO', includePrePost: false, events: 'div|split|earn' };
+
+    //     const chart = await got(chartUrl, { searchParams })
+    //             .then(res =>
+    //              {
+    //     if (res)
+    //     {
+    //         return JSON.parse(res.body).chart.result;
+    //     }
+    // })
+    //             .catch (err => {
+    //             if (err && err.response)
+    //             {
+    //                 console.log(err.response.body);
+    //                 return err.response.body;
+    //             }
+    //         });
+
+    // if (chart && chart.length > 0)
+    // {
+    //     return chart[0];
+    // }
+
+    // return { };
+    //         }
+
+    //         savePortfolio(portfolio: Portfolio) {
+    //     if (!portfolio) return;
+
+    //     const portfolioPath = path.resolve('./data/portfolio_allegutta.json');
+    //     const backupPortfolioPath = path.resolve('./data/portfolio_allegutta_backup_' + new Date().valueOf() + '.json');
+    //     fs.copyFileSync(portfolioPath, backupPortfolioPath);
+    //     fs.writeFileSync(portfolioPath, JSON.stringify(portfolio));
+    //     portfolio = portfolio;
+    //     return portfolio;
+    // }
+}
