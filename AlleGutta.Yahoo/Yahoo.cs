65368b48a4c9ebdabc67cb1d49074c82efc634e7
André Biseth <andre@biseth.net>
2023-06-05 05:54:29 +0000

    Added function to obtain crumb Will need more processing before success

--- AlleGutta.Yahoo/Yahoo.cs
+++ AlleGutta.Yahoo/Yahoo.cs
@@ -1,19 +1,66 @@
-﻿using System.Web;
+﻿using System.Net;
+using System.Web;
 using AlleGutta.Models.Yahoo;
 using Newtonsoft.Json;
 
 namespace AlleGutta.Yahoo;
 public sealed class YahooApi
 {
+    private readonly string scrapeUrl;
+    private readonly string crumbUrl;
     private readonly string quotesUrl;
     private readonly string chartUrl;
     private readonly string optionsUrl;
 
     public YahooApi()
     {
-        quotesUrl = "https://query2.finance.yahoo.com/v6/finance/quote";
+        scrapeUrl = "https://finance.yahoo.com/quote/%5EGSPC/options";
+        crumbUrl = "https://query1.finance.yahoo.com/v1/test/getcrumb";
+        quotesUrl = "https://query2.finance.yahoo.com/v7/finance/quote";
         chartUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
         optionsUrl = "https://query2.finance.yahoo.com/v7/finance/options/";
+    }
+
+    private async Task<string> GetCrumbCookie()
+    {
+        var builder = new UriBuilder(scrapeUrl)
+        {
+            Port = -1
+        };
+        var query = HttpUtility.ParseQueryString(builder.Query);
+        builder.Query = query.ToString();
+        string url = builder.ToString();
+
+        var cookies = new CookieContainer();
+        using var handler = new HttpClientHandler { CookieContainer = cookies };
+        using var client = new HttpClient(handler);
+        var response = await client.GetAsync(url);
+        var responseCookies = cookies.GetCookies(builder.Uri).Cast<Cookie>();
+
+        return responseCookies.FirstOrDefault(c => c.Name == "B")?.Value ?? string.Empty;
+    }
+
+    private async Task<string> GetCrumb()
+    {
+        var builder = new UriBuilder(crumbUrl)
+        {
+            Port = -1
+        };
+        var query = HttpUtility.ParseQueryString(builder.Query);
+        query["formatted"] = "false";
+        query["lang"] = "nb-NO";
+        query["region"] = "NO";
+        query["corsDomain"] = "finance.yahoo.com";
+        builder.Query = query.ToString();
+        string url = builder.ToString();
+
+        var cookieContainer = new CookieContainer();
+        using var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+        using var client = new HttpClient(handler);
+        cookieContainer.Add(builder.Uri, new Cookie("B", await GetCrumbCookie()));
+        var response = await client.GetStringAsync(url);
+
+        return response ?? string.Empty;
     }
 
     public async Task<IEnumerable<QuoteResult>> GetQuotes(IEnumerable<string> tickers)
@@ -31,6 +78,7 @@ public sealed class YahooApi
         query["symbols"] = tickers.Any() ? tickers.Select(x => x).Aggregate((x, y) => $"{x},{y}") : string.Empty;
         query["fields"] = "shortName,longName,regularMarketChange,regularMarketChangePercent,regularMarketTime,regularMarketPrice,regularMarketDayHigh,regularMarketDayRange,regularMarketDayLow,regularMarketVolume,regularMarketPreviousClose";
         query["corsDomain"] = "finance.yahoo.com";
+        query["crumb"] = await GetCrumb();
         builder.Query = query.ToString();
         string url = builder.ToString();
 
