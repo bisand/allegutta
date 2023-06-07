07797df87beb5cf388874daaf9ffdbea7ab7405d
André Biseth <andre@biseth.net>
2023-06-07 18:49:38 +0000

    #3 FIxed crumb issue for now

--- AlleGutta.Yahoo/Yahoo.cs
+++ AlleGutta.Yahoo/Yahoo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Web;
 using AlleGutta.Models.Yahoo;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace AlleGutta.Yahoo;
@@ -11,56 +12,17 @@ public sealed class YahooApi
     private readonly string quotesUrl;
     private readonly string chartUrl;
     private readonly string optionsUrl;
+    private readonly ILogger _logger;
 
-    public YahooApi()
+    public YahooApi(ILoggerFactory loggerFactory)
     {
         scrapeUrl = "https://finance.yahoo.com/quote/%5EGSPC/options";
         crumbUrl = "https://query1.finance.yahoo.com/v1/test/getcrumb";
         quotesUrl = "https://query2.finance.yahoo.com/v7/finance/quote";
         chartUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
         optionsUrl = "https://query2.finance.yahoo.com/v7/finance/options/";
-    }
-
-    private async Task<string> GetCrumbCookie()
-    {
-        var builder = new UriBuilder(scrapeUrl)
-        {
-            Port = -1
-        };
-        var query = HttpUtility.ParseQueryString(builder.Query);
-        builder.Query = query.ToString();
-        string url = builder.ToString();
-
-        var cookies = new CookieContainer();
-        using var handler = new HttpClientHandler { CookieContainer = cookies };
-        using var client = new HttpClient(handler);
-        var response = await client.GetAsync(url);
-        var responseCookies = cookies.GetCookies(builder.Uri).Cast<Cookie>();
-
-        return responseCookies.FirstOrDefault(c => c.Name == "B")?.Value ?? string.Empty;
-    }
-
-    private async Task<string> GetCrumb()
-    {
-        var builder = new UriBuilder(crumbUrl)
-        {
-            Port = -1
-        };
-        var query = HttpUtility.ParseQueryString(builder.Query);
-        query["formatted"] = "false";
-        query["lang"] = "nb-NO";
-        query["region"] = "NO";
-        query["corsDomain"] = "finance.yahoo.com";
-        builder.Query = query.ToString();
-        string url = builder.ToString();
-
-        var cookieContainer = new CookieContainer();
-        using var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
-        using var client = new HttpClient(handler);
-        cookieContainer.Add(builder.Uri, new Cookie("B", await GetCrumbCookie()));
-        var response = await client.GetStringAsync(url);
-
-        return response ?? string.Empty;
+        CrumbManager.Logger = loggerFactory.CreateLogger("AlleGutta.Yahoo.CrumbManager");
+        _logger = loggerFactory.CreateLogger<YahooApi>();
     }
 
     public async Task<IEnumerable<QuoteResult>> GetQuotes(IEnumerable<string> tickers)
@@ -78,13 +40,18 @@ public sealed class YahooApi
         query["symbols"] = tickers.Any() ? tickers.Select(x => x).Aggregate((x, y) => $"{x},{y}") : string.Empty;
         query["fields"] = "shortName,longName,regularMarketChange,regularMarketChangePercent,regularMarketTime,regularMarketPrice,regularMarketDayHigh,regularMarketDayRange,regularMarketDayLow,regularMarketVolume,regularMarketPreviousClose";
         query["corsDomain"] = "finance.yahoo.com";
-        query["crumb"] = await GetCrumb();
+        query["crumb"] = await CrumbManager.GetCrumbAsync();
         builder.Query = query.ToString();
         string url = builder.ToString();
 
-        using var client = new HttpClient();
-        var response = await client.GetStringAsync(url);
-        var quotes = JsonConvert.DeserializeObject<QuoteQyeryResult>(response, new[] { new InvalidDataFormatJsonConverter() });
+        using var client = CrumbManager.GetHttpClient();
+        using var request = new HttpRequestMessage(HttpMethod.Get, url);
+        CrumbManager.FillHeaders(request, "https://finance.yahoo.com/quote/OSEBX.OL");
+
+        using var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var quotes = JsonConvert.DeserializeObject<QuoteQyeryResult>(responseBody, new[] { new InvalidDataFormatJsonConverter() });
 
         return quotes?.QuoteResponse?.Result ?? Array.Empty<QuoteResult>();
     }
